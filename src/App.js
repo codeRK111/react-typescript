019803942fea8786d9eab83621c0e46374969632
rakesh <rakesh.das@idslogic.com>
2019-05-28 11:04:45 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [number, changeNumber] = useState(0);
+  const onClick = e => {
+    let _number = number + 1;
+    changeNumber(_number);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="number" value={number} />
+      <button onClick={ onClick }>Add</button>
     </div>
   );
 }
