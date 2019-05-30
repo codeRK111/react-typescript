17fc7642e0d820ab567bc86fa7634b97e7043b17
rakesh <rakesh.das@idslogic.com>
2019-05-30 11:34:08 +0530

    Latest changes from master

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() : JSX.Element {
       <input type="number" value={number} />
       <button onClick={onClick}>Increase on dev</button>
       <h1>This is done by ts</h1>
+      <h1>This is done by ts (master)</h1>
     </div>
   );
 }
